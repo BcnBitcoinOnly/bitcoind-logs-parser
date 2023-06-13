ac79fc341d89f3b836aa65b16ec448cf7a9859dd
Marcel Hernandez <e6990620@gmail.com>
2023-06-13 23:21:50 +0200

    don't fail on mixed block logs nor missing debug lines

--- parser.php
+++ parser.php
@@ -12,48 +12,57 @@ $f = fopen($argv[1], 'r');
 echo 'timestamp,height,hash,version,log2_work,compact_size_bytes,prefilled_txs,mempool_txs,extra_mempool_txs,requested_txs,validation_time' . PHP_EOL;
 
 while(($line = fgets($f)) !== false) {
-    if (empty($data) && preg_match('/^(?<timestamp>.+) Initialized PartiallyDownloadedBlock for block (?<hash>[0-9a-f]+) using a cmpctblock of size (?<size>\d+)/', $line, $matches)) {
-        $data = [
-            'start' => $matches['timestamp'],
-            'hash' => $matches['hash'],
-            'compact_size' => (int) $matches['size']
-        ];
+    if (preg_match('/^(?<timestamp>.+) Initialized PartiallyDownloadedBlock for block (?<hash>[0-9a-f]+) using a cmpctblock of size (?<size>\d+)/', $line, $matches)) {
+        $hash = $matches['hash'];
+
+        if (!isset($data[$hash])) {
+            $data[$hash] = [
+                'start' => $matches['timestamp'],
+                'compact_size' => (int) $matches['size']
+            ];
+        }
 
         continue;
     }
 
     if (preg_match('/^(?<timestamp>.+) Successfully reconstructed block (?<hash>[0-9a-f]+) with (?<prefilled>\d+) txn prefilled, (?<mempool>\d+) txn from mempool \(incl at least (?<extra>\d+) from extra pool\) and (?<requested>\d+) txn requested/', $line, $matches)) {
-        $data['prefilled'] = (int) $matches['prefilled'];
-        $data['mempool'] = (int) $matches['mempool'];
-        $data['extra'] = (int) $matches['extra'];
-        $data['requested'] = (int) $matches['requested'];
+        $hash = $matches['hash'];
+
+        $data[$hash]['prefilled'] = (int) $matches['prefilled'];
+        $data[$hash]['mempool'] = (int) $matches['mempool'];
+        $data[$hash]['extra'] = (int) $matches['extra'];
+        $data[$hash]['requested'] = (int) $matches['requested'];
 
         continue;
     }
 
     if (preg_match('/^(?<timestamp>.+) UpdateTip: new best=(?<hash>[0-9a-f]+) height=(?<height>\d+) version=(?<version>0x[0-9a-f]+) log2_work=(?<work>\d+\.\d+)/', $line, $matches)) {
-        $data['end'] = $matches['timestamp'];
-        $data['height'] = (int) $matches['height'];
-        $data['version'] = $matches['version'];
-        $data['log2_work'] = (float) $matches['work'];
+        $hash = $matches['hash'];
 
-        $data['validation_time'] = (new DateTimeImmutable($data['end']))->getTimestamp() - (new DateTimeImmutable($data['start']))->getTimestamp();
+        $data[$hash]['end'] = $matches['timestamp'];
+        $data[$hash]['height'] = (int) $matches['height'];
+        $data[$hash]['version'] = $matches['version'];
+        $data[$hash]['log2_work'] = (float) $matches['work'];
+
+        if (isset($data[$hash]['start'])) {
+            $data[$hash]['validation_time'] = (new DateTimeImmutable($data[$hash]['end']))->getTimestamp() - (new DateTimeImmutable($data[$hash]['start']))->getTimestamp();
+        }
 
         echo sprintf(
-            '%s,%d,%s,%s,%f,%d,%d,%d,%d,%d,%d' . PHP_EOL,
-            $data['end'],
-            $data['height'],
-            $data['hash'],
-            $data['version'],
-            $data['log2_work'],
-            $data['compact_size'],
-            $data['prefilled'],
-            $data['mempool'],
-            $data['extra'],
-            $data['requested'],
-            $data['validation_time']
+            '%s,%d,%s,%s,%f,%s,%s,%s,%s,%s,%s' . PHP_EOL,
+            $data[$hash]['end'],
+            $data[$hash]['height'],
+            $hash,
+            $data[$hash]['version'],
+            $data[$hash]['log2_work'],
+            $data[$hash]['compact_size'] ?? 'NULL',
+            $data[$hash]['prefilled'] ?? 'NULL',
+            $data[$hash]['mempool'] ?? 'NULL',
+            $data[$hash]['extra'] ?? 'NULL',
+            $data[$hash]['requested'] ?? 'NULL',
+            $data[$hash]['validation_time'] ?? 'NULL'
         );
 
-        $data = [];
+        unset($data[$hash]);
     }
 }
